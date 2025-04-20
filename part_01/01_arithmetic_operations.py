cf07657e987a24521a9ae111a8fed70615bea431
Renaldo Freire <orenaldo@proton.me>
2025-04-20 03:14:30 -0300

    adicionado novas aulas e tasks

---
+++ part_01/01_arithmetic_operations.py
@@ -0,0 +1,18 @@
+
+altura = float(input("Qual sua altura? "))
+peso = float(input("Qual seu peso? "))
+
+imc = peso / (altura / 100) ** 2
+
+print(f"O seu IMC é {imc}")
+
+# Divisão / - ponto flutuante
+# Divisão // - inteiro
+x = 3
+y = 2
+
+# print(f"/ operator {x/y}")
+# print(f"// operator {x//y}")
+
+# year = int(input("Qual o seu ano de nascimento? "))
+# print(f"Sua idade no ano de 2021 era: {2021 - year}")
