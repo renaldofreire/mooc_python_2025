4336cda7cc365859efd2d1c5f541373b0f669a46
Renaldo Freire <orenaldo@proton.me>
2025-04-24 09:22:10 -0300

    realizada novas atividades

--- part_01/part1/part1_task_conditional_statements.py
+++ part_01/part1/part1_task_conditional_statements.py
@@ -14,4 +14,47 @@
 
 # Programming exercise:
 # Soup or no soup
+# name = input("Please tell me your name:")
+# portion_price = 5.90
+#
+# if name != "Jerry":
+#     num_portions = float(input("How many portions of soup?"))
+#     print(f"The total cost is {num_portions * portion_price}")
+# print("Next please!")
+
+# Programming exercise:
+# Order of magnitude
+# number = int(input("Please type in a number: "))
+#
+# if number <= 999:
+#     print("This number is smaller than 1000")
+#
+# if number <= 99:
+#     print("This number is smaller than 100")
+#
+# if number <= 9:
+#     print("This number is smaller than 10")
+#
+# print("Thank you!")
+
+# Programming exercise:
+# Calculator
+num1 = int(input("Number 1: "))
+num2 = int(input("Number 2: "))
+operation = input("Operation: ")
+
+if operation == "add":
+    print(f"{num1} + {num2} = {num1+num2}")
+
+if operation == "multiply":
+    print(f"{num1} * {num2} = {num1*num2}")
+
+if operation == "subtract":
+    print(f"{num1} - {num2} = {num1-num2}")
+
+print("")
+
+
+
+
 
