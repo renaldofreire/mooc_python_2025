0aa973c26c098404b1ea1baf3895fde9cfc8ab95
Renaldo Freire <orenaldo@proton.me>
2025-05-06 08:52:49 -0300

    conclui exercício e finalizei a parte 1 do curso

--- part_01/part1/part1_task_students_in_groups.py
+++ part_01/part1/part1_task_students_in_groups.py
@@ -1,4 +1,21 @@
 # Programming exercise
 # Students in groups
 
+studants = int(input("How many students on the course? "))
+groups_size = int(input("Desired group size? "))
 
+groups_formed = studants // groups_size
+
+if studants % groups_size != 0:
+    groups_formed = groups_formed + 1
+
+print(f"Number of groups formed: {groups_formed}")
+
+# Model solution is much better:
+# students = int(input("How many students on the course? "))
+#
+# group_size = int(input("Desired group size? "))
+#
+# groups = (students + group_size - 1) // group_size
+#
+# print("Number of groups formed:", groups)
