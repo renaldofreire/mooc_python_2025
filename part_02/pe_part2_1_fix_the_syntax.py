1af782cf06da8d6538acdd9b07ca0cae1458c442
Renaldo Freire <orenaldo@proton.me>
2025-05-13 09:52:34 -0300

    .

--- part_02/pe_part2_1_fix_the_syntax.py
+++ part_02/pe_part2_1_fix_the_syntax.py
@@ -4,9 +4,9 @@ number = int(input("Please type in a number: "))
 
 if number > 100:
     print("The number was greater than one hundred")
+    print("Now its value has decreased by one hundred")
     number -= 100
+    print("Its value is now", number)
 
-print("Now its value has decreased by one hundred")
-print("Its value is now", number)
 print(number, " must be my lucky number!")
 print("Have a nice day!")
