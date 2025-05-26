61adbf86ae65fff344c5c017628717246daadfa8
Renaldo Freire <orenaldo@proton.me>
2025-05-26 15:46:38 -0300

    .

---
+++ part_02/pe_part2_2_number_of_characters.py
@@ -0,0 +1,15 @@
+# Programming exercise:
+# Number of characters
+
+word = "abcd"
+print(len(word))
+
+print(len("hi there"))
+
+word2 = "howdydoody"
+length = len(word2)
+print(length)
+
+empty_string = ""
+length = len(empty_string)
+print(length)
