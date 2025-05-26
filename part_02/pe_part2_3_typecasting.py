5cf50f5a8b6e9fffe7c10f0ba6ea8cd3760bb594
Renaldo Freire <orenaldo@proton.me>
2025-05-26 16:27:08 -0300

    .

--- part_02/pe_part2_3_typecasting.py
+++ part_02/pe_part2_3_typecasting.py
@@ -1,2 +1,11 @@
 # Programming exercise:
 # Typecasting
+
+number = float(input("Please type in a number: "))
+
+integer_part = int(number)
+decimal_part = float(number) - int(number)
+
+print("Integer part:", integer_part)
+print("Decimal part:", decimal_part)
+
