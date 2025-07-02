7cfc3772c582d95ef43ce65ea6a1b789ca74dabd
Renaldo Freire <orenaldo@proton.me>
2025-07-02 19:54:28 -0300

    finish PE from part2: The next leap year

---
+++ part_02/pe_part2_20_the_next_leap_year.py
@@ -0,0 +1,17 @@
+# Programming exercise:
+# The next leap year
+
+year = int(input("Year: "))
+
+candidate_year = year + 1
+
+while True:
+    is_divisible_by_400 = (candidate_year % 400 == 0)
+    is_divisible_by_4_but_not_100 = (candidate_year % 4 == 0 and candidate_year % 100 !=0)
+
+    if is_divisible_by_400 or is_divisible_by_4_but_not_100:
+        print(f"The next leap year after {year} is {candidate_year}")
+        break
+
+    candidate_year += 1
+
