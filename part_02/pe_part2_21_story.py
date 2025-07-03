469bcb1d6c1e8a844a115a2358e236a1131110d3
Renaldo Freire <orenaldo@proton.me>
2025-07-03 12:47:37 -0300

    PE: Complete: Story

---
+++ part_02/pe_part2_21_story.py
@@ -0,0 +1,17 @@
+# Programming exercise:
+# Story
+
+story = ""
+previous_word = ""
+
+while True:
+    word = input("Please type in a word: ")
+
+    if word == "end" or word == previous_word:
+        break
+
+    story += word + " "
+    previous_word = word
+
+print(story)
+
